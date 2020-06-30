ab7e3a3b59b6f59b682bb3bb98331db8ef953c32
Terry chan <napoler2008@gmail.com>
2020-06-30 20:46:05 +0800

    Signed-off-by: Terry chan <napoler2008@gmail.com>

--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,9 @@
 # animal_data
 动物数据
+
+
+## 导入到数据
+
+```
+python run.py 
+```
---
+++ run.py
@@ -0,0 +1,34 @@
+import json
+import pymongo
+import os
+client = pymongo.MongoClient('localhost')
+db = client['animal'] #数据库名
+# collection = db['images']
+def add_col(name,path):
+    """
+    name：表名字
+    path:json文件目
+    
+    """
+    with open(path, encoding="utf-8") as jf:
+        str = jf.read()
+        data = []
+        data.extend(json.loads(str))
+        collection = db[name]
+        if len(data)>0:
+            collection.insert_many(data)
+    
+
+
+
+# 数据所在目录
+path='data'
+for pathname,dirnames,filenames in os.walk(path):
+    for filename in filenames:
+        file=os.path.join(pathname,filename)
+        if filename.endswith("json"):
+            print(file,filename.replace(".json",''))
+            name=filename.replace(".json",'')
+            add_col(name,file)
+print("已经全部导入！")
+client.close() #写完关闭连接
